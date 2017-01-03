d8b4d969bffe3507300b365b9e33c02fb0c829df
Rob <rdaly1490@gmail.com>
2017-01-03 15:46:56 -0600

    save to local storage and add event delegation for checkbox toggle

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
   const addItems = document.querySelector('.add-items');
   const itemsList = document.querySelector('.plates');
-  const items = [];
+  const items = JSON.parse(localStorage.getItem('items')) || [];
 
   function addItem(e) {
   	e.preventDefault();
@@ -11,6 +11,7 @@
   	};
   	items.push(item);
   	populateList(items, itemsList);
+  	localStorage.setItem('items', JSON.stringify(items));
   	this.reset();
   }
 
@@ -27,4 +28,17 @@
   	htmlList.innerHTML = listElements;
   }
 
+  function toggleDone(e) {
+  	if (!e.target.matches('input')) return;
+  	const el = e.target;
+  	const index = el.dataset.index;
+  	items[index].done = !items[index].done;
+  	localStorage.setItem('items', JSON.stringify(items));
+  	populateList(items, itemsList);
+  }
+
   addItems.addEventListener('submit', addItem);
+  itemsList.addEventListener('click', toggleDone);
+
+  populateList(items, itemsList);
+
